323302b0a08a8a47cdab223694c50f8ca09eece3
Jennifer Beaver <jen.lee.beaver@gmail.com>
2019-12-27 13:56:13 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -9,11 +9,13 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-
+  num1 / num2 = quotient
+  puts quotient 
 end
 
 def multiplication(num1, num2)
-
+  num1 * num2 = product 
+  puts product 
 end
 
 def modulo(num1, num2)
