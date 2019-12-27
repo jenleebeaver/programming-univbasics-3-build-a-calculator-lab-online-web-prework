fc9efd13582017a75dddc1c7212ba9d607e5a4df
Jennifer Beaver <jen.lee.beaver@gmail.com>
2019-12-27 14:00:42 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -15,12 +15,12 @@ end
 
 def multiplication(num1, num2)
   num1 * num2 = product
-  puts product 
+  puts product
 end
 
 def modulo(num1, num2)
   num2 / num1 = remainder
-  puts remainder 
+  puts remainder
 end
 
 def square_root(num)
